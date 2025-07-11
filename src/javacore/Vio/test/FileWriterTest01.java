e56413f24fcce545f96c97ce616913a568ea37d5
GabrielOliveira <gabrieloliveira96@outlook.com>
2025-07-11 16:55:56 -0300

    initial commit

--- src/javacore/Vio/test/FileWriterTest01.java
+++ src/javacore/Vio/test/FileWriterTest01.java
@@ -3,8 +3,13 @@ package academy.devdojo.maratonajava.javacore.Vio.test;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+//File
+//FileWriter
+//FileReader
+//BufferedWriter
+//BufferedReader
 
-public class FileWriter01 {
+public class FileWriterTest01 {
     public static void main(String[] args) {
         File file  = new File ("file.txt");
         try (FileWriter fw = new FileWriter(file, true)){
