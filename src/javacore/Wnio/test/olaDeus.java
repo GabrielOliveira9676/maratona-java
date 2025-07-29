d510c1054ed556dfef5c979f8103724e0cfe897b
GabrielOliveira <gabrieloliveira96@outlook.com>
2025-07-29 18:10:12 -0300

    initial commit

---
+++ src/javacore/Wnio/test/olaDeus.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+public class olaDeus {
+    public static void main(String[] args) {
+
+        String Deus;
+        Deus = "Ola meu Deus";
+        System.out.println(Deus);
+
+    }
+
+
+}
